edc7a170a626cdf0c1a2c21c90159988bf3c58dd
Viktoriia Kutseva <Kutsevaviktoriia@gmail.com>
2024-11-11 20:34:14 +0600

    upd

--- app/main.py
+++ app/main.py
@@ -1,20 +1,33 @@
 import os, sys
 sys.dont_write_bytecode = True
 sys.path.append(os.getcwd())
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import uvicorn
 from routers.cat_router import cat_router
+from pathlib import Path
 
 
 
 app = FastAPI()
 
+# Constructing the absolute path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(BASE_DIR, "static")
+
+# Mounting the static directory
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+templates_dir = os.path.join(os.path.dirname(__file__), "templates")
+templates = Jinja2Templates(directory=templates_dir)
+
 app.include_router(cat_router, prefix="/cats")
 
 @app.get("/")
-def read_root():
-    return {'message': 'Добро пожаловать в КотоВыбор'}
+async def read_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/cats")
-def read_cats():
-    return [{"id": 1, "name": "Whiskers"}, {"id": 2, "name": "Mittens"}]
+# @app.get("/cats")
+# def read_cats():
+#     return [{"id": 1, "name": "Whiskers"}, {"id": 2, "name": "Mittens"}]
